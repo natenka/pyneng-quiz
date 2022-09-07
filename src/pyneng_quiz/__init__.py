3bd1b4019e816959b2d0c2460903f20af844fa3e
pyneng <pyneng.course@gmail.com>
2022-09-07 07:32:37 +0000

    fixed qa loading error

--- src/pyneng_quiz/__init__.py
+++ src/pyneng_quiz/__init__.py
@@ -2,6 +2,6 @@ from importlib import resources
 import json
 
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
-ALL_QUESTIONS = json.loads(resources.read_text("pyneng", "questions.json"))
+ALL_QUESTIONS = json.loads(resources.read_text("pyneng_quiz", "questions.json"))
